0eb31be338aafdd7154ebde8a091ec5827477229
SojiRaju <136330579+SojiRaju@users.noreply.github.com>
2023-07-13 17:05:50 +0530

    13/07/2023 commit

---
+++ ptt/refresh_token.py
@@ -0,0 +1,23 @@
+import requests
+
+def access_token():
+  # To get Access Token
+  payload = {"username": "anisha", "password": "Anisha@12345"}
+  url = "https://dev-api.pttapp.com/api/"
+  response = requests.post(url + "login", json=payload)
+  data = response.json()
+  token = data["authenticationResult"]["AccessToken"]
+  return token
+access_token()
+def refresh(token, url):
+  payload = {"refreshToken":""}
+  headers = {
+    'Authorization': f'Bearer {access_token()}'
+  }
+  print(payload)
+  response = requests.post(url + "refresh", json=payload, headers=headers)
+  if response.status_code == 200:
+    print(f"success: {response.json()}")
+
+  else:
+    print(f"Error: {response.status_code}")
